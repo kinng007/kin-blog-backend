f26147d58be07b297a4b8bcc4d7e3bc5c381a04b
kinng007 <34981734+kinng007@users.noreply.github.com>
2021-09-19 15:27:59 +0530

    remove logging

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,12 +1,7 @@
 import { Config } from "./types";
 import dotenv from "dotenv";
-import path from "path";
-
-//dotenv.config({ path: path.join(__dirname, "..", "..", "..", ".env") });
 
 dotenv.config({ path: "/etc/app/.env" });
-
-console.log(process.env);
 
 const config: Config = {
   port: (process.env.PORT && Number.parseInt(process.env.PORT)) || 3000,
